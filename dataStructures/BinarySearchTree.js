a2cfec8c3fc5d64c5592e52fff4726ddba4d757f
JonathanLGeorge <jgeorge12@horizon.csueastbay.edu>
2019-08-12 22:17:55 -0700

    travesal + height

--- dataStructures/BinarySearchTree.js
+++ dataStructures/BinarySearchTree.js
@@ -125,6 +125,128 @@ class BST {
         }
         this.root = removeNode(this.root, data);
     }
+
+    isBalanced() {
+        return (this.findMinHeight() >= this.findMaxHeight() - 1)
+      }
+
+    findMinHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        }
+        let left = this.findMinHeight(node.left);
+        let right = this.findMinHeight(node.right);
+        if (left < right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        }
+    }
+
+    findMinHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        };
+        let left = this.findMinHeight(node.left);
+        let right = this.findMinHeight(node.right);
+        if (left < right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
+
+    findMaxHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        };
+        let left = this.findMaxHeight(node.left);
+        let right = this.findMaxHeight(node.right);
+        if (left > right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
+
+    findMaxHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        }
+        let left = this.findMaxHeight(node.left);
+        let right = this.findMaxHeight(node.right);
+        if (left > right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        }
+    }
+
+    preOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePreOrder(node) {
+            result.push(node.data);
+            node.left && traversePreOrder(node.left);
+            node.right && traversePreOrder(node.right);
+          }
+          traversePreOrder(this.root);
+          return result;
+        }
+      }
+
+      inOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traverseInOrder(node) {       
+            node.left && traverseInOrder(node.left);
+            result.push(node.data);
+            node.right && traverseInOrder(node.right);
+          }
+          traverseInOrder(this.root);
+          return result;
+        }
+      }
+
+    postOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePostOrder(node) {
+            node.left && traversePostOrder(node.left);
+            node.right && traversePostOrder(node.right);
+            result.push(node.data);
+          }
+          traversePostOrder(this.root);
+          return result;
+        }
+      }
+
+    levelOrder() {
+        let result = [];
+        let Q = []; 
+        if (this.root != null) {
+            Q.push(this.root);
+            while(Q.length > 0) {
+                let node = Q.shift();
+                result.push(node.data);
+                if (node.left != null) {
+                    Q.push(node.left);
+                }
+                if (node.right != null) {
+                    Q.push(node.right);
+                }
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
 }
 
 //testing out our data structure
@@ -139,8 +261,18 @@ bst.add(5);
 bst.add(7);
 
 bst.remove(6);
-console.log(bst.findMax);
-console.log(bst.findMin);
-console.log(bst.findMax);
+console.log(bst.findMax());
+console.log(bst.findMin());
+bst.remove(7);
+console.log(bst.findMax());
 console.log(bst.isPresent(6));
 console.log(bst.isPresent(2));
+
+console.log(bst.isBalanced());
+
+console.log('min hieght', bst.findMinHeight());
+console.log('inOrder: ', bst.inOrder());
+
+console.log('preOrder: ', bst.preOrder());
+
+console.log('postOrder: ', bst.postOrder());
